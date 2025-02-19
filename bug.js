5e783c8e995beeb5f1a9369ca0acf0dfbf2b3488
BugHunter-X <askzenai@gmail.com>
2025-02-19 18:16:03 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+While building an Expo project, encountering an error related to the unexpected token 'export' in the Metro bundler. This typically occurs when attempting to import or use modules that contain ES modules syntax, while the Metro bundler is configured to use a different module system like CommonJS.
